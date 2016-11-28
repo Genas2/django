76774567ef05e704bba52c7141b1871cf2dd35b0
genas2 <Gennas2@gmail.com>
2016-11-28 07:43:56 +0100

    Added choices to the Question editing in admin panel.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,6 +4,17 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 
