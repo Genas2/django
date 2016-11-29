9c23ad0b3500bb35aec0e850a1bb8e972161a728
genas2 <gennas2@gmail.com>
2016-11-29 23:33:34 +0100

    Implemented dynamic extra value for ChoiceInline model.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,9 +4,17 @@ from .models import Question, Choice
 
 # Register your models here.
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
+
+    def get_extra(self, request, obj=None, **kwargs):
+        if obj == None:
+            return self.extra
+        elif obj.choice_set.count() >= 3:
+            return 1
+        else:
+            return self.extra - obj.choice_set.count()
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
